52b24daba2f2343122f295035cf76ab19e4ed197
NolwenMaj <115166022+NolwenMaj@users.noreply.github.com>
2023-01-12 10:49:35 +0100

    ajout partie to do

---
+++ toDo.js
@@ -0,0 +1,30 @@
+var ul = document.getElementsByTagName("ul")[0];
+var myList = document.getElementsByTagName("li");
+var myListSpan = document.getElementsByTagName("span");
+
+function addToDo() {
+    var todo = document.getElementById("todo").value;
+    if (todo ==="") alert("You must write something !");
+    else {
+        document.getElementById("todo").value = "";
+        var li = document.createElement("li");
+        li.innerHTML = todo;
+        li.addEventListener("click", checked);
+        var span = document.createElement("span");
+        span.innerHTML = "x";
+        span.addEventListener("click", deleteToDo);
+        li.appendChild(span);
+        ul.appendChild(li);
+    }
+}
+
+function checked() {
+    var classLi = this.getAttribute("class")
+    if(classLi === "checked") this.setAttribute("class", "unchecked");
+    else this.setAttribute("class", "checked");
+}
+
+function deleteToDo() {
+    ul.removeChild(this.parentElement);
+}
+
