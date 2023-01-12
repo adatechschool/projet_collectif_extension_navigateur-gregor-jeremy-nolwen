ba55685b2b65a71887b3aec5dec99d04244d1d3c
21Gregor <bechetgregor5@gmail.com>
2023-01-12 10:38:16 +0100

    commit pour rebase

--- toDo.js
+++ toDo.js
@@ -1,6 +1,6 @@
 var ul = document.getElementsByTagName("ul")[0];
-var MyList = document.getElementsByTagName("li");
-var MyListSpan = document.getElementsByTagName("span");
+var myList = document.getElementsByTagName("li");
+var myListSpan = document.getElementsByTagName("span");
 
 function addToDo() {
     var todo = document.getElementById("todo").value;
@@ -9,9 +9,22 @@ function addToDo() {
         document.getElementById("todo").value = "";
         var li = document.createElement("li");
         li.innerHTML = todo;
+        li.addEventListener("click", checked);
         var span = document.createElement("span");
         span.innerHTML = "x";
+        span.addEventListener("click", deleteToDo);
         li.appendChild(span);
         ul.appendChild(li);
     }
 }
+
+function checked() {
+    var classLi = this.getAttribute("class")
+    if(classLi === "checked") this.setAttribute("class", "unchecked");
+    else this.setAttribute("class", "checked");
+}
+
+function deleteToDo() {
+    ul.removeChild(this.parentElement);
+}
+
